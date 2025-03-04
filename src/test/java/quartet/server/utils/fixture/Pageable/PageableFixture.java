a858c68791fcc69829433b0843994439b8e0d971
choijohy <gitjeehee7948@git.vhost>
2025-03-04 18:30:50 +0900

    [QUAR-33] ✅ Test: 컨벤션 통일(api 버저닝, 컨트롤러 응답 데이터 비교 방식)

--- src/test/java/quartet/server/utils/fixture/Pageable/PageableFixture.java
+++ src/test/java/quartet/server/utils/fixture/Pageable/PageableFixture.java
@@ -2,13 +2,14 @@ package quartet.server.utils.fixture.Pageable;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 
 public class PageableFixture {
     public static Pageable pageable(){
-        return PageRequest.of(0, 10);
+        return PageRequest.of(0, 10, Sort.by(Sort.Order.asc("id")));
     }
-    public static Pageable pageable(int page, int pageSize) {
-        return PageRequest.of(page, pageSize);
+    public static Pageable pageable(int page, int pageSize, Sort sort) {
+        return PageRequest.of(page, pageSize, sort);
     }
 }
