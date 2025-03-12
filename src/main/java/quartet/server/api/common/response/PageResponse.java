96fd9bbc7d80e29618abe39ebee7d7332bc04cd2
sor999 <sorsor999@naver.com>
2025-03-13 03:23:25 +0900

    [QUAR-23] ♻️ Refactor: Page Response를 record로 변경

--- src/main/java/quartet/server/api/common/response/PageResponse.java
+++ src/main/java/quartet/server/api/common/response/PageResponse.java
@@ -1,33 +1,28 @@
 package quartet.server.api.common.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.data.domain.Page;
-
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class PageResponse<T> {
-    private List<T> content;
-    private int page;
-    private int pageSize;
-    private long totalElements;
-    private int totalPages;
-    private boolean first;
-    private boolean last;
-    private boolean empty;
-
+public record PageResponse<T>(
+        List<T> content,
+        int page,
+        int pageSize,
+        long totalElements,
+        int totalPages,
+        boolean first,
+        boolean last,
+        boolean empty
+) {
     public static <T> PageResponse<T> from(Page<T> page) {
         return new PageResponse<>(
-            page.getContent(),
-            page.getNumber(),
-            page.getSize(),
-            page.getTotalElements(),
-            page.getTotalPages(),
-            page.isFirst(),
-            page.isLast(),
-            page.isEmpty()
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast(),
+                page.isEmpty()
         );
     }
 }
