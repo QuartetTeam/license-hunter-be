710cc2ef347cb70e94aecdab277d0c90a0a64b1e
김정윤 <canach3@gmail.com>
2025-04-21 15:21:08 +0900

    Update OauthLoginSuccessHandler.java

--- src/main/java/quartet/server/core/security/handler/OauthLoginSuccessHandler.java
+++ src/main/java/quartet/server/core/security/handler/OauthLoginSuccessHandler.java
@@ -43,7 +43,8 @@ public class OauthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHand
 
         response.addCookie(createCookie("accessToken", accessToken, 60 * 10));
         response.addCookie(createCookie("refreshToken", refreshToken, 24 * 60 * 60));
-        response.sendRedirect("http://localhost:5173/api/v1/oauth2-jwt-header");
+        getRedirectStrategy().sendRedirect(request, response,
+                "http://localhost:5173/api/v1/oauth2-jwt-header?accessToken=" + accessToken + "&refreshToken=" + refreshToken);
     }
 
     public static Cookie createCookie(String key, String value, Integer expiredS) {
