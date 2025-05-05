dac55db76be24818fafb2f5478a52e6d22c15b5d
HyunJe Park <sorsor999@naver.com>
2025-05-05 20:36:19 +0900

    Example 관련 클래스 삭제시 배포 안되는 문제 해결 (#115)
    * [QUAR-65] ✨ Feat: mailing, calendar 임시 memberId를 헤더에서 가져오는 것으로 변경
    
    * [QUAR-65] 🔥 Remove: Example 관련 클래스 삭제
    
    * [QUAR-65] 🔥 Remove: 주석 삭제
    
    * [QUAR-65] 🔥 Remove: ExampleException import문 삭제

--- src/main/java/quartet/server/domain/mail/exception/MailingNotFoundException.java
+++ src/main/java/quartet/server/domain/mail/exception/MailingNotFoundException.java
@@ -1,7 +1,6 @@
 package quartet.server.domain.mail.exception;
 
 import quartet.server.core.code.MailErrorCode;
-import quartet.server.domain.example.exception.ExampleException;
 
 public class MailingNotFoundException extends MailException {
     public MailingNotFoundException() {
