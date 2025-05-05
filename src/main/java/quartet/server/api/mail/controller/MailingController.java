96a1632637d2f0b377f13e93a9f88b65c4caa31a
sor999 <sorsor999@naver.com>
2025-05-05 16:59:05 +0900

    [QUAR-65] ✨ Feat: mailing, calendar 임시 memberId를 헤더에서 가져오는 것으로 변경

--- src/main/java/quartet/server/api/mail/controller/MailingController.java
+++ src/main/java/quartet/server/api/mail/controller/MailingController.java
@@ -3,11 +3,14 @@ package quartet.server.api.mail.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import quartet.server.api.common.response.ApiResponse;
 import quartet.server.api.common.response.PageResponse;
 import quartet.server.api.mail.dto.response.MailingResponse;
 import quartet.server.api.mail.service.MailingService;
+import quartet.server.core.security.userDetails.CustomUserDetails;
 
 import java.util.List;
 
@@ -21,24 +24,25 @@ public class MailingController {
     private final MailingService mailingService;
 
     @GetMapping("/mailings")
-    public ApiResponse<PageResponse<MailingResponse>> getMemberMailing(
+    public ApiResponse<PageResponse<MailingResponse>> getMemberMailing(@AuthenticationPrincipal final CustomUserDetails userDetails,
                                                                        @RequestParam(defaultValue = "0") final int page,
                                                                        @RequestParam(defaultValue = "4") final int pageSize) {
-        long memberId = 1L; // TODO: @AuthenticationPrincipal로 변경 예정
-        return ApiResponse.success(OK, PageResponse.from(mailingService.getMailingsByMemberId(memberId, PageRequest.of(page, pageSize))));
+        return ApiResponse.success(OK, PageResponse.from(mailingService.getMailingsByMemberId(userDetails.getMemberId(), PageRequest.of(page, pageSize))));
     }
 
     @PostMapping("/certifications/{certificationId}/mailings")
-    public ApiResponse<MailingResponse> subscribeMailing(@PathVariable("certificationId") final long certificationId) {
-        long memberId = 1L; // TODO: @AuthenticationPrincipal로 변경 예정
-        mailingService.subscribeMailing(memberId, certificationId);
+    public ApiResponse<MailingResponse> subscribeMailing(
+            @AuthenticationPrincipal final CustomUserDetails userDetails,
+            @PathVariable("certificationId") final long certificationId) {
+        mailingService.subscribeMailing(userDetails.getMemberId(), certificationId);
         return ApiResponse.success(CREATED);
     }
 
     @DeleteMapping("/mailings")
-    public ApiResponse<Void> unsubscribeMailings(@RequestParam final List<Long> mailingIds) {
-        long memberId = 1L; // TODO: @AuthenticationPrincipal로 변경 예정
-        mailingService.unsubscribeMailings(memberId, mailingIds);
+    public ApiResponse<Void> unsubscribeMailings(
+            @AuthenticationPrincipal final CustomUserDetails userDetails,
+            @RequestParam final List<Long> mailingIds) {
+        mailingService.unsubscribeMailings(userDetails.getMemberId(), mailingIds);
         return ApiResponse.success(NO_CONTENT);
     }
 }
